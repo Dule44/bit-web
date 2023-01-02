4327ef249dc389a03d45bdd61cfba0a370d607bb
Dusan <aleks29444@gmail.com>
2023-01-02 17:53:05 +0100

    commit

---
+++ DOM/Traversing/index.js
@@ -0,0 +1,8 @@
+(function () {
+  var list = document.querySelector(".active");
+
+  list.classList.remove("active");
+
+  list.parentElement.parentElement.parentElement.firstElementChild.firstElementChild.firstElementChild.className =
+    "active";
+})();
